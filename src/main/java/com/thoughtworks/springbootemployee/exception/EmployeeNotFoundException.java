6527b946561abcdc25bc338c209e208af24141b5
kudoyuto <yuto.kudo@oocl.com>
2021-08-13 16:10:40 +0800

    [mockMVCHomeWork][Yuto] feat: added exception handling and Errorresponse class

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    private String message;
+    public EmployeeNotFoundException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
